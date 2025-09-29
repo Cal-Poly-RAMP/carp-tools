4006e25cebd66abecd4e24c4d81a84858d3c53aa
ryancramuh <ryancramer22@gmail.com>
2025-09-28 17:20:15 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,48 +1,52 @@
 #!/usr/bin/env bash
 
+# Get Current Directory
 ABS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" && pwd )
 
+# Select Local or Remote Container
 if [ "$1" = "-l" ] || [ "$1" = "--local" ]; then
-    CONTAINER=carp-tools:latest
-else
-    CONTAINER=ryancramer22/carp-tools:latest
+    CONTAINER=asic
+else 
+    CONTAINER=fwilken/asic:alpha
 fi
 
 TERM=xterm-256color
 HOST=asic
 
 if [ ! -d $ABS_DIR/workspace ]; then
-    mkdir -p $ABS_DIR/workspace
-	cp Makefile workspace/
-	cp -r scripts/ workspace/
+	mkdir -p $ABS_DIR/workspace
 fi
 
-# Mac
+
+# Mac Install
 if [[ "$OSTYPE" == "darwin"* ]]; then
     set -x
-    xhost -; xhost +localhost
+
+    xhost -
+    xhost +localhost
+
     docker run -it --rm \
         -v $ABS_DIR/workspace:/home/$HOST/workspace:rw \
-        -v ~/.ssh:/home/$HOST/.ssh \
+        -v ~/.ssh:/home/$HOST/.ssh\
         -v ~/.gitconfig:/home/$HOST/.gitconfig \
         -e DISPLAY=host.docker.internal:0 \
-        -e "TERM=$TERM" \
+        -e "TERM=$TERM"\
         --hostname $HOST \
         $CONTAINER -s bash
 
-# Linux / WSL
-else
+# Linux/WSL Install
+else 
     set -x
     xhost local:root
     docker run -it --rm \
-        -v $ABS_DIR/workspace:/home/$HOST/workspace:rw \
-        -v ~/.ssh:/home/$HOST/.ssh \
-        -v ~/.gitconfig:/home/$HOST/.gitconfig \
-        -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v /mnt/wslg:/mnt/wslg \
-        -e DISPLAY \
-        -e "TERM=$TERM" \
-        --hostname $HOST \
-        --net=host \
-        $CONTAINER -s /bin/bash
+                -v $ABS_DIR/workspace:/home/$HOST/workspace:rw\
+                -v ~/.ssh:/home/$HOST/.ssh\
+                -v ~/.gitconfig:/home/$HOST/.gitconfig \
+                -v /tmp/.X11-unix:/tmp/.X11-unix \
+                -v /mnt/wslg:/mnt/wslg \
+                -e DISPLAY \
+                -e "TERM=$TERM"\
+                --hostname $HOST \
+                --net=host \
+                $CONTAINER -s /bin/bash
 fi
